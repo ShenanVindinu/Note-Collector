51dae362538bbd81dc13495ee021cf4b842c87b9
ShenanVindinu <shenanvindinu@gmail.com>
2024-09-07 13:19:11 +0530

    NoteController created

---
+++ src/main/java/lk/ijse/gdse/aad67/notecollector/controller/NoteController.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse.aad67.notecollector.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/notes")
+public class NoteController {
+
+
+
+}
