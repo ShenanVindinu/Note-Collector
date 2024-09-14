e5e1601f3eff8a4aa2ed443bbfbd7f6c65932286
ShenanVindinu <shenanvindinu@gmail.com>
2024-09-14 15:18:55 +0530

    User Entity created

---
+++ src/main/java/lk/ijse/gdse/aad67/notecollector/entity/impl/UserEntity.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse.aad67.notecollector.entity.impl;
+
+import jakarta.persistence.*;
+import lk.ijse.gdse.aad67.notecollector.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class UserEntity implements SuperEntity {
+
+    @Id
+    private String userId;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String profilePic;
+
+    @OneToMany(mappedBy = "user")
+    private List<NoteEntity> notes;
+}
