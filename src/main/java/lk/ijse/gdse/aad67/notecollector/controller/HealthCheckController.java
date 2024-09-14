f77417deca6319f2482757fe3c9669764c2e0c2c
ShenanVindinu <shenanvindinu@gmail.com>
2024-09-14 09:30:13 +0530

    health check created

---
+++ src/main/java/lk/ijse/gdse/aad67/notecollector/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse.aad67.notecollector.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/health")
+public class HealthCheckController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String healthCheck() {
+        return "Note controller is working";
+    }
+}
