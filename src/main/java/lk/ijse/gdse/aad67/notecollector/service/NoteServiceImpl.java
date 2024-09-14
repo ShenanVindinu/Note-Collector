76a42a8c77e7c532a6e9669945dd99c6924c5e02
ShenanVindinu <shenanvindinu@gmail.com>
2024-09-14 11:11:51 +0530

    Constructor created

--- src/main/java/lk/ijse/gdse/aad67/notecollector/service/NoteServiceImpl.java
+++ src/main/java/lk/ijse/gdse/aad67/notecollector/service/NoteServiceImpl.java
@@ -4,11 +4,25 @@ import lk.ijse.gdse.aad67.notecollector.dto.impl.NoteDTO;
 import lk.ijse.gdse.aad67.notecollector.util.AppUtil;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class NoteServiceImpl implements NoteService {
+
+    private static List<NoteDTO> notes = new ArrayList<>();
+
+    public NoteServiceImpl() {
+        notes.add(new NoteDTO(
+                "Note2",
+                "Sample Title",
+                "sample desc",
+                "2024.09.07",
+                "P1",
+                "2"
+        ));
+    }
+
     @Override
     public NoteDTO saveNote(NoteDTO noteDTO) {
         noteDTO.setNoteId(AppUtil.generateNoteId());
