cb23a517eb680e4f965cf7c0371ce59adfc03763
ShenanVindinu <shenanvindinu@gmail.com>
2024-09-14 09:28:12 +0530

    Note Service interface created

---
+++ src/main/java/lk/ijse/gdse/aad67/notecollector/service/NoteService.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad67.notecollector.service;
+
+import lk.ijse.gdse.aad67.notecollector.dto.impl.NoteDTO;
+
+import java.util.List;
+
+public interface NoteService {
+    String saveNote(NoteDTO noteDTO);
+    List<NoteDTO> getAllNotes();
+    NoteDTO getNote(String noteId);
+    boolean deleteNote(String noteId);
+    boolean updateNote(NoteDTO noteDTO);
+}
