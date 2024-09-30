ca91e814b6e1849073c4a55d5ee4d822dd82313a
ShenanVindinu <shenanvindinu@gmail.com>
2024-09-30 12:49:48 +0530

    code reformatted

--- src/main/java/lk/ijse/gdse/aad67/notecollector/service/NoteService.java
+++ src/main/java/lk/ijse/gdse/aad67/notecollector/service/NoteService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface NoteService {
     NoteDTO saveNote(NoteDTO noteDTO);
+
     List<NoteDTO> getAllNotes();
+
     NoteDTO getNote(String noteId);
+
     boolean deleteNote(String noteId);
+
     boolean updateNote(NoteDTO noteDTO);
 }
